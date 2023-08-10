416c44f7b2fb60b3bd0e42bb3abe0a0535e387a0
Zachey <fradexo56@gmail.com>
2023-08-11 00:23:31 +0500

    Add blog

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express();
 const env = process.env;
 
 const homeRoute = require("./src/routes/home");
+const blogRoute = require("./src/routes/blog");
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 app.use(express.static("public"));
 
 app.use("/", homeRoute);
+app.use("/blog", blogRoute);
 
 app.listen(env.port, () => {
   console.log(`App listening on port ${env.port}`);
