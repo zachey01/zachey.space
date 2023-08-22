e816b7d7f65a7e636e6b94d003f2e5d00bb37cb1
Zachey <fradexo56@gmail.com>
2023-08-23 00:11:47 +0500

    Add mimicms website

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const compression = require("compression");
+const subdomain = require("express-subdomain");
 const https = require("https");
 const fs = require("fs");
 const ejs = require("ejs");
@@ -10,11 +11,50 @@ let blogRoute = require("./src/routes/blog");
 let projectsRoute = require("./src/routes/projects");
 let contactsRoute = require("./src/routes/contacts");
 let donateRoute = require("./src/routes/donate");
+let mimicmsSubDomain = express.Router();
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 app.use(express.static("public", { dotfiles: "allow" }));
 app.use(compression());
+
+mimicmsSubDomain.get("/", (req, res) => {
+  res.render("mimicms");
+});
+
+mimicmsSubDomain.get("/files", (req, res) => {
+  const imgFolderPath = path.join(__dirname, "./public/mimicms/mimicms");
+
+  function getFilesFromFolder(folderPath, parentPath = "") {
+    let files = [];
+
+    const folderContents = fs.readdirSync(folderPath, { withFileTypes: true });
+
+    folderContents.forEach((item) => {
+      const itemPath = path.join(folderPath, item.name);
+      const itemRelativePath = path.join(parentPath, item.name);
+
+      if (item.isFile()) {
+        files.push(itemRelativePath);
+      } else if (item.isDirectory()) {
+        const subFolderFiles = getFilesFromFolder(itemPath, itemRelativePath);
+        files = files.concat(subFolderFiles);
+      }
+    });
+
+    return files;
+  }
+
+  try {
+    const allFiles = getFilesFromFolder(imgFolderPath);
+    res.json({ files: allFiles });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to read directory" });
+  }
+});
+
+app.use(subdomain("mimicms", mimicmsSubDomain));
 
 app.use("/", homeRoute);
 app.use("/blog", blogRoute);
