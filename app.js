6a6e5a94fc9f3cdb9f3403e6767a8357e7a21696
Zachey <fradexo56@gmail.com>
2023-09-10 14:07:11 +0500

    Remove MimiCMS

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ let blogRoute = require("./src/routes/blog");
 let projectsRoute = require("./src/routes/projects");
 let contactsRoute = require("./src/routes/contacts");
 let donateRoute = require("./src/routes/donate");
-let mimicmsSubDomain = require("./src/routes/mimicms");
 let apiSubDomain = require("./src/routes/api");
 let placeholderSubDomain = require("./src/routes/placeholder");
 
@@ -23,7 +22,6 @@ app.use(express.static("public", { dotfiles: "allow" }));
 app.use(compression());
 app.use(cors());
 
-app.use(subdomain("mimicms", mimicmsSubDomain));
 app.use(subdomain("api", apiSubDomain));
 app.use(subdomain("placeholder", placeholderSubDomain));
 
--- src/routes/api.js
+++ src/routes/api.js
@@ -87,4 +87,36 @@ router.get("/steamworkshop/:steamid", (req, res) => {
   workshopparser(`${req.params.steamid}`, res);
 });
 
+router.get("/mimicms-files", (req, res) => {
+  const imgFolderPath = path.join(__dirname, "../../public/mimicms");
+
+  function getFilesFromFolder(folderPath, parentPath = "") {
+    let files = [];
+
+    const folderContents = fs.readdirSync(folderPath, { withFileTypes: true });
+
+    folderContents.forEach((item) => {
+      const itemPath = path.join(folderPath, item.name);
+      const itemRelativePath = path.join(parentPath, item.name);
+
+      if (item.isFile()) {
+        files.push(itemRelativePath);
+      } else if (item.isDirectory()) {
+        const subFolderFiles = getFilesFromFolder(itemPath, itemRelativePath);
+        files = files.concat(subFolderFiles);
+      }
+    });
+
+    return files;
+  }
+
+  try {
+    const allFiles = getFilesFromFolder(imgFolderPath);
+    res.json({ files: allFiles });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to read directory" });
+  }
+});
+
 module.exports = router;
